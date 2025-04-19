3325aa2b99806ea5dc585683d71788916480a585
andche742 <andche742@gmail.com>
2025-04-19 16:17:15 -0700

    implemented word box

--- client/app/product/page.jsx
+++ client/app/product/page.jsx
@@ -1,14 +1,20 @@
 "use client";
 
+import WordBox from "../../components/wordbox.jsx";
 import { Button } from "../../components/button.jsx";
+import Navbar from "@/components/navbar.jsx";
 
 export default function testPage(){
-    return <>
-    
-    
-    
+    const text = "Generated text goes here! Hello, 123";
+    return (
+    <>
+        <Navbar></Navbar>
         <div>test</div>
-        <Button>blabhalbh</Button>
+        <div className="flex justify-evenly items-center border border-gray-300 rounded-md p-4">
+            <div className="flex">
+                <WordBox text={text} />
+            </div>
+        </div>
     </>
+    )
 }
-
