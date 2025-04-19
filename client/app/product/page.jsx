3f3da4e00f28d55a45c6af9c3ca9f82fca3a347e
andche742 <andche742@gmail.com>
2025-04-19 15:15:13 -0700

    created product page

---
+++ client/app/product/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "../../components/button.jsx";
+
+export default function testPage(){
+    return <>
+    
+    
+    
+        <div>test</div>
+        <Button>blabhalbh</Button>
+    </>
+}
+
